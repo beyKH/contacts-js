5a9406cd0b89364ffcdd1f5f862ca7b625b9e166
Asadbek Makhmudjonov <72538664+AsadbekMakhmudjonov@users.noreply.github.com>
2021-08-30 14:56:00 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var elInput = document.querySelector(".js-input");
+var elOutput = document.querySelector(".js-output");
+var elSubmitBtn = document.querySelector(".js-submit");
+var elForm = document.querySelector(".js-form");
+
+elForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+})
